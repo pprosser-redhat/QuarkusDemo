2c05a97ae87101c36391b00dfb931347e782511f
Phil Prosser <pprosser@redhat.com>
2023-03-13 14:00:05 +0000

    added amqp support

--- rest-hero/src/main/java/io/quarkus/workshop/super_heroes/HeroService.java
+++ rest-hero/src/main/java/io/quarkus/workshop/super_heroes/HeroService.java
@@ -5,8 +5,18 @@ import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
 
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import io.vertx.core.json.JsonObject;
+
 @ApplicationScoped
 public class HeroService {
+
+    @Channel("new-hero") Emitter<Hero> heroEmitter;
+    
     
     public List<Hero> findAllHeros() {
 
@@ -21,11 +31,24 @@ public class HeroService {
     @Transactional
     public Hero createHero(Hero hero){
         Hero.persist(hero);
+
+        heroEmitter.send(hero);
+
         return hero;
     }
 
     @Transactional
     public void deleteHero(Long id) {
         Hero.deleteById(id);
+    }
+
+    @Transactional
+    @Incoming("hero-event")
+    public void receiveHero(JsonObject heroEvent) {
+        System.out.println(heroEvent.toString());
+
+        // Contruct Object from JSON
+        Hero hero = heroEvent.mapTo(Hero.class);
+        System.out.println(hero.toString());
     }
 }
